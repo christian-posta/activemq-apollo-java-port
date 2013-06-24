6bcdcfb29ca4ca303b6b49aa86bc5ffc214db1dd
Christian Posta <christian.posta@gmail.com>
2013-06-24 01:29:31 -0400

    Fixed potential issue with DTO tests which I found in Apollo scala

--- apollo-util/src/test/java/org/apache/activemq/apollo/dto/DtoModule2.java
+++ apollo-util/src/test/java/org/apache/activemq/apollo/dto/DtoModule2.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.activemq.apollo.util.test;
+package org.apache.activemq.apollo.dto;
 
 import org.apache.activemq.apollo.util.DtoModule;
 
@@ -24,7 +24,7 @@ import org.apache.activemq.apollo.util.DtoModule;
 public class DtoModule2 implements DtoModule {
     @Override
     public String dtoPackage() {
-        return "org.apache.activemq.apollo.util.test";
+        return "org.apache.activemq.apollo.dto";
 
     }
 
